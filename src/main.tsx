b25f831308b2a05073ad3dcf9cf8d7bc744a8f7e
lyoz <1482802+lyoz@users.noreply.github.com>
2022-04-13 00:51:31 +0900

    remove files

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import AppOld from "./AppOld";
+
+const App = () => <div>Hello, world.</div>;
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <AppOld />
   </React.StrictMode>,
   document.getElementById("root")
 );
