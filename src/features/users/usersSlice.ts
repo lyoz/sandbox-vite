44ee626c0bcb80a0ad25e7c0ba8995761a75f1d5
lyoz <1482802+lyoz@users.noreply.github.com>
2022-05-31 06:30:59 +0900

    fetch users

--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -1,15 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { client } from "../../client";
 
-const initialState = [
-  { id: "0", name: "Tianna Jenkins" },
-  { id: "1", name: "Kevin Grant" },
-  { id: "2", name: "Madison Price" },
-];
+export type User = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  name: string;
+  username: string;
+};
+
+const initialState: User[] = [];
+
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  const response = await client.get("/fakeApi/users");
+  return response.data as User[];
+});
 
 const usersSlice = createSlice({
   name: "users",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload;
+    });
+  },
 });
 
 export default usersSlice.reducer;
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
+import { fetchUsers } from "./features/users/usersSlice";
 
 (async () => {
   if (process.env.NODE_ENV === "development") {
     const { worker } = await import("./mocks/browser");
     await worker.start();
   }
+
+  store.dispatch(fetchUsers());
+
   ReactDOM.render(
     <React.StrictMode>
       <Provider store={store}>
