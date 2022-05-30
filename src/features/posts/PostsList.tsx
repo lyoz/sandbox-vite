1e471eeab612f802bb4c3f287777eed8fc2468e3
lyoz <1482802+lyoz@users.noreply.github.com>
2022-05-31 03:16:31 +0900

    refactor

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -2,9 +2,22 @@ import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { PostAuthor } from "./PostAuthor";
-import { fetchPosts, selectAllPosts } from "./postsSlice";
+import { selectAllPosts, fetchPosts, Post } from "./postsSlice";
 import { ReactionButtons } from "./ReactionButtons";
 import { TimeAgo } from "./TimeAgo";
+
+const PostExcerpt = ({ post }: { post: Post }) => {
+  return (
+    <article>
+      <h3>{post.title}</h3>
+      <PostAuthor userId={post.userId} />
+      <TimeAgo timestamp={post.date} />
+      <p>{post.content}</p>
+      <ReactionButtons post={post} />
+      <Link to={`/posts/${post.id}`}>View Post</Link>
+    </article>
+  );
+};
 
 export const PostsList = () => {
   const dispatch = useAppDispatch();
@@ -18,23 +31,22 @@ export const PostsList = () => {
     }
   }, [dispatch, postStatus]);
 
-  const orderedPosts = [...posts].sort(({ date: a }, { date: b }) =>
-    b.localeCompare(a)
-  );
-  const renderedPosts = orderedPosts.map((post) => (
-    <article key={post.id}>
-      <h3>{post.title}</h3>
-      <PostAuthor userId={post.userId} />
-      <TimeAgo timestamp={post.date} />
-      <p>{post.content}</p>
-      <ReactionButtons post={post} />
-      <Link to={`/posts/${post.id}`}>View Post</Link>
-    </article>
-  ));
+  let content;
+  if (postStatus === "loading") {
+    content = <div>loading...</div>;
+  } else {
+    const orderedPosts = [...posts].sort(({ date: a }, { date: b }) =>
+      b.localeCompare(a)
+    );
+    content = orderedPosts.map((post) => (
+      <PostExcerpt key={post.id} post={post} />
+    ));
+  }
+
   return (
     <section>
       <h2>PostsList</h2>
-      <div>{renderedPosts}</div>
+      {content}
     </section>
   );
 };
