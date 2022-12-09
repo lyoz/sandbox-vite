9636c4fcb7e336d3cab0088993400c56388a0162
lyoz <1482802+lyoz@users.noreply.github.com>
2022-12-09 23:26:58 +0900

    update all packages

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
+
   return (
     <>
       <p>Hello, Vite + React + TypeScript!</p>
       <button onClick={() => setCount((count) => count + 1)}>
-        count is: {count}
+        count is {count}
       </button>
     </>
   );
