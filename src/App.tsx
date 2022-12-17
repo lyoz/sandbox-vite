bb6976a8b600f71b1aca9fb531bc69811835c726
lyoz <1482802+lyoz@users.noreply.github.com>
2022-12-17 16:41:42 +0900

    handling not found errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ErrorPage } from "./ErrorPage";
 import { Root } from "./routes/Root";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
---
+++ src/ErrorPage.tsx
@@ -0,0 +1,18 @@
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
+export const ErrorPage = () => {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      {isRouteErrorResponse(error) && (
+        <p>
+          <i>{error.statusText}</i>
+        </p>
+      )}
+    </div>
+  );
+};
