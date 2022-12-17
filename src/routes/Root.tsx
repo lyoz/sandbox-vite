21e745aa086ca9fa26a2d8c94a5b8a8a319b10d7
lyoz <1482802+lyoz@users.noreply.github.com>
2022-12-17 21:20:20 +0900

    active link styling

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,4 +1,10 @@
-import { Form, Link, Outlet, redirect, useLoaderData } from "react-router-dom";
+import {
+  Form,
+  NavLink,
+  Outlet,
+  redirect,
+  useLoaderData,
+} from "react-router-dom";
 import { createContact, getContacts } from "../contacts";
 
 export const loader = async () => {
@@ -41,7 +47,12 @@ export const Root = () => {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive ? "active" : isPending ? "pending" : ""
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -50,7 +61,7 @@ export const Root = () => {
                       <i>No Name</i>
                     )}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
