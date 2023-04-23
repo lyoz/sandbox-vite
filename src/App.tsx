d9d7126deda7f32cb6061028339d7afff89a9474
lyoz <1482802+lyoz@users.noreply.github.com>
2023-04-23 21:22:40 +0900

    format

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
 export const App = () => {
-  const [count, setCount] = useState(0);
+	const [count, setCount] = useState(0);
 
-  return (
-    <div>
-      <p>Hello, Vite + React + TypeScript!</p>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-    </div>
-  );
+	return (
+		<div>
+			<p>Hello, Vite + React + TypeScript!</p>
+			<button onClick={() => setCount((count) => count + 1)}>
+				count is {count}
+			</button>
+		</div>
+	);
 };
--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ if (!container) throw new Error("Failed to find the root element");
 
 const root = createRoot(container);
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+	<StrictMode>
+		<App />
+	</StrictMode>,
 );
