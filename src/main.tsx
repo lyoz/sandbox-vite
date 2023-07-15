8648ff94d5eb0936228de73d23268353b9449be7
lyoz <1482802+lyoz@users.noreply.github.com>
2023-07-15 19:16:04 +0900

    add minimal codes for redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { App } from "./App";
+import { store } from "./app/store";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
@@ -8,6 +10,8 @@ if (!container) throw new Error("Failed to find the root element");
 const root = createRoot(container);
 root.render(
 	<StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</StrictMode>,
 );
