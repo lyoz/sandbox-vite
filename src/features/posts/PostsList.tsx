c11bd7fa7b7f48c25c686313cd63c1aa24c08e51
lyoz <1482802+lyoz@users.noreply.github.com>
2023-07-17 22:55:09 +0900

    displaying loading state

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -1,15 +1,29 @@
-import { useEffect } from "react";
+import { ReactNode, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { PostAuthor } from "./PostAuthor";
 import { ReactionButtons } from "./ReactionButtons";
 import { TimeAgo } from "./TimeAgo";
-import { fetchPosts } from "./postsSlice";
+import { Post, fetchPosts } from "./postsSlice";
+
+const PostExcerpt = ({ post }: { post: Post }) => (
+	<article>
+		<h3>{post.title}</h3>
+		<div style={{ display: "flex", columnGap: 8 }}>
+			<PostAuthor userId={post.userId} />
+			<TimeAgo createdAt={post.createdAt} />
+		</div>
+		<p>{post.content.substring(0, 100)}</p>
+		<ReactionButtons post={post} />
+		<Link to={`/posts/${post.id}`}>View Post</Link>
+	</article>
+);
 
 export const PostsList = () => {
 	const dispatch = useAppDispatch();
 	const posts = useAppSelector((state) => state.posts.posts);
 	const postStatus = useAppSelector((state) => state.posts.status);
+	const error = useAppSelector((state) => state.posts.error);
 
 	useEffect(() => {
 		if (postStatus === "idle") {
@@ -17,25 +31,24 @@ export const PostsList = () => {
 		}
 	}, [dispatch, postStatus]);
 
-	const orderedPosts = [...posts].sort((a, b) =>
-		b.createdAt.localeCompare(a.createdAt),
-	);
+	let content: ReactNode = null;
+	if (postStatus === "loading") {
+		content = <div>Loading...</div>;
+	} else if (postStatus === "succeeded") {
+		const orderedPosts = [...posts].sort((a, b) =>
+			b.createdAt.localeCompare(a.createdAt),
+		);
+		content = orderedPosts.map((post) => (
+			<PostExcerpt key={post.id} post={post} />
+		));
+	} else if (postStatus === "failed") {
+		content = <div>{error}</div>;
+	}
 
 	return (
 		<section>
 			<h2>Posts</h2>
-			{orderedPosts.map((post) => (
-				<article key={post.id}>
-					<h3>{post.title}</h3>
-					<div style={{ display: "flex", columnGap: 8 }}>
-						<PostAuthor userId={post.userId} />
-						<TimeAgo createdAt={post.createdAt} />
-					</div>
-					<p>{post.content.substring(0, 100)}</p>
-					<ReactionButtons post={post} />
-					<Link to={`/posts/${post.id}`}>View Post</Link>
-				</article>
-			))}
+			{content}
 		</section>
 	);
 };
