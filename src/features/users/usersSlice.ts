45150739ba965e8c20e976e9e5934687beb356e1
lyoz <1482802+lyoz@users.noreply.github.com>
2023-08-07 01:41:28 +0900

    transforming responses

--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -1,4 +1,8 @@
-import { createSelector } from "@reduxjs/toolkit";
+import {
+	EntityState,
+	createEntityAdapter,
+	createSelector,
+} from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
 import { apiSlice } from "../api/apiSlice";
 
@@ -7,37 +11,30 @@ export type User = {
 	name: string;
 };
 
-/* Temporarily ignore adapter
 const usersAdapter = createEntityAdapter<User>();
 
 type UsersState = EntityState<User>;
 
 const initialState: UsersState = usersAdapter.getInitialState();
-*/
 
 export const extendedApiSlice = apiSlice.injectEndpoints({
 	endpoints: (builder) => ({
-		getUsers: builder.query<User[], void>({
+		getUsers: builder.query<UsersState, void>({
 			query: () => "users",
+			transformResponse: (responseData: User[]) =>
+				usersAdapter.setAll(initialState, responseData),
 		}),
 	}),
 });
 
 export const selectUsersResult = extendedApiSlice.endpoints.getUsers.select();
 
-const emptyUsers: User[] = [];
-
-export const selectAllUsers = createSelector(
+const selectUsersData = createSelector(
 	selectUsersResult,
-	(usersResult) => usersResult.data ?? emptyUsers,
+	(usersResult) => usersResult.data,
 );
 
-export const selectUserById = createSelector(
-	[selectAllUsers, (_: RootState, userId: string) => userId],
-	(users, userId) => users.find((user) => user.id === userId),
-);
-
-/* Temporarily ignore selectors
 export const { selectAll: selectAllUsers, selectById: selectUserById } =
-	usersAdapter.getSelectors((state: RootState) => state.users);
-*/
+	usersAdapter.getSelectors(
+		(state: RootState) => selectUsersData(state) ?? initialState,
+	);
